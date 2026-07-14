8725df8ac9c736a1dc32c1a076f4be9bb0ae24df
Arunav Mehta <arunav232006@gmail.com>
2026-07-14 10:58:15 +0530

    Final: Fix Sentry Config

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,11 @@
 import { withSentryConfig } from "@sentry/nextjs";
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  experimental: {
+    serverComponentsExternalPackages: ["@sentry/nextjs"]
+  }
+};
 
 export default withSentryConfig(nextConfig, {
   silent: true,
